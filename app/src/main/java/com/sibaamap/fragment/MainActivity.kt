b05707b108af449e9cb9dd07e7b3fe5eae6192df
sibaamap <nguyenhuyanh2510@gmail.com>
2022-06-03 14:31:30 +0700

    replaceFragment

---
+++ app/src/main/java/com/sibaamap/fragment/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.sibaamap.fragment
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.sibaamap.fragment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fragment1Btn.setOnClickListener {
+            replaceFragment(Fragment1())
+
+        }
+
+        binding.fragment2Btn.setOnClickListener {
+            replaceFragment(Fragment2())
+        }
+
+
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer,fragment)
+        fragmentTransaction.commit()
+
+    }
+}
